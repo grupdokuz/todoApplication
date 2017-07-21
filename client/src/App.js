ca0c2d7d579cd9b70b93ea9233eecc69aea73295
mkutlu <mkutlu@etu.edu.tr>
2017-07-21 18:54:45 +0300

    Basic index created.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor () {
+    super()
+    this.state = {}
+    this.getTodos = this.getTodos.bind(this)
+    this.getTodo = this.getTodo.bind(this)
+  }
+  componentDidMount () {
+    this.getTodos()
+  }
+  fetch (endpoint) {
+    return new Promise((resolve, reject) => {
+      window.fetch(endpoint)
+      .then(response => response.json())
+      .then(json => resolve(json))
+      .catch(error => reject(error))
+    })
+  }
+  getTodos () {
+    this.fetch('api/todos')
+      .then(todos => {
+        this.setState({todos: todos})
+        this.getTodo(todos[0].id)
+      })
+  }
+  getTodo (id) {
+    this.fetch(`api/todos/${id}`)
+      .then(todo => this.setState({todo: todo}))
+  }
+  render () {
+    let {todos, todo} = this.state
+    return todos
+    ? <Container text>
+        <Header as='h2' icon textAlign='center'>
+        <Icon name='numbered list' circular />
+        <Header.Content>
+          Todo List
+        </Header.Content>
+      </Header>
+      <Button.Group fluid widths={todos.length}>
+        {Object.keys(todos).map((key) => {
+          return <Button active={todo && todo.id === todos[key].id} fluid key={key} onClick={() => this.getTodo(todos[key].id)}>
+            {todos[key].title}
+          </Button>
+        })}
+      </Button.Group>
+      <Divider hidden />
+      {todo &&
+        <Container>
+          <Header as='h2'>{todo.title}</Header>
+          {todo.created_by && <p>{todo.created_by}</p>}
+          {todo.items &&
+            <Segment.Group>
+              {todo.items.map((item, i) => <Segment key={i}>{item.name}</Segment>)}
+            </Segment.Group>
+          }
+        </Container>
+      }
+    </Container>
+    : <Container text>
+      <Dimmer active inverted>
+        <Loader content='Loading' />
+      </Dimmer>
+    </Container>
   }
 }
 
-export default App;
+export default App
