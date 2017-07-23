c7c501f6f79ad96bd1c069d6df3985086d5cb1d1
EnesAltinisik <es.altinisik@gmail.com>
2017-07-23 12:21:16 +0300

    route file change

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   scope '/api' do
-        resources :todos
+        resources :todos do
+	 resources :items 
+	end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
